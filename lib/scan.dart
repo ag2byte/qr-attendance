0160220c46d2cfe2a82f142323058e33d10e6a78
ag2byte1 <capsand123@outlook.com>
2021-04-17 01:57:26 +0530

    [added] fetching through heroku

--- lib/scan.dart
+++ lib/scan.dart
@@ -1,65 +1,68 @@
 import 'package:barcode_scan/barcode_scan.dart';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 class ScanPage extends StatefulWidget {
   @override
-  _ScanPageState createState() =>
-      _ScanPageState();
+  _ScanPageState createState() => _ScanPageState();
 }
-class _ScanPageState extends
-State<ScanPage> {
+
+class _ScanPageState extends State<ScanPage> {
+  
+
   String qrCodeResult = "Scan The QR for Attendance";
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
+      appBar: AppBar(
         title: Text("Scanner"),
         centerTitle: true,
-         ),
-        body: Container(
+      ),
+      body: Container(
         padding: EdgeInsets.all(20.0),
         child: Column(
-        mainAxisAlignment:
-        MainAxisAlignment.center,
-        crossAxisAlignment:
-        CrossAxisAlignment.stretch,
-        children: <Widget>[
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            // the below was the test for http package
+            // TextButton(
+            //     onPressed: () async {
+            //       http.Response response = await http.get('https://qrbackbone.herokuapp.com/'); //getting the response from our backend server script
+            //       print(response.body);
+            //     },
+            //     child: Text('Get api call')),
+
             Text(
-            qrCodeResult,
-            style: TextStyle(
-            fontSize: 20.0,
-            ),
-            textAlign: TextAlign.center,
+              qrCodeResult,
+              style: TextStyle(
+                fontSize: 20.0,
+              ),
+              textAlign: TextAlign.center,
             ),
             SizedBox(
-            height: 20.0,
+              height: 20.0,
             ),
             FlatButton(
               padding: EdgeInsets.all(15.0),
-              onPressed: ()
-              async {
-              String codeScanner = await BarcodeScanner.scan();
-              setState(() {
-              qrCodeResult = codeScanner;
-              });
-            },
+              onPressed: () async {
+                String codeScanner = await BarcodeScanner.scan();
+                setState(() {
+                  qrCodeResult = codeScanner;
+                });
+              },
               child: Text(
                 "Open Scanner",
                 style:
-                TextStyle(color:
-                Colors.blue, fontWeight: FontWeight.bold),
+                    TextStyle(color: Colors.blue, fontWeight: FontWeight.bold),
               ),
-              shape:
-              RoundedRectangleBorder(
-                  side: BorderSide(color:
-                  Colors.blue, width: 3.0),
-                  borderRadius:
-                  BorderRadius.circular(20.0)),
+              shape: RoundedRectangleBorder(
+                  side: BorderSide(color: Colors.blue, width: 3.0),
+                  borderRadius: BorderRadius.circular(20.0)),
             )
-        ],
+          ],
         ),
-        ),
+      ),
     );
   }
-
 }
